e81137b063b59b1e90761aea1bcfb2bc26cd8af5
Do Nhat Minh <mrordinaire@gmail.com>
2016-01-21 13:43:58 +0800

    replaced + 1 and - 1 with inc and dec

--- src/psswrd_solver/selection.clj
+++ src/psswrd_solver/selection.clj
@@ -5,18 +5,18 @@
   ([^booleans array]
    (last-false-index-of array (alength array)))
   ([^booleans array ^Integer upto-excluding]
-   (loop [idx (- upto-excluding 1)]
+   (loop [idx (dec upto-excluding)]
      (cond
        (< idx 0) nil
        (= false (aget array idx)) idx
-       :else (recur (- idx 1))))))
+       :else (recur (dec idx))))))
 
 (defn first-false-index-of [^booleans array]
   (loop [idx 0]
      (cond
        (= idx (alength array)) nil
        (= false (aget array idx)) idx
-       :else (recur (+ idx 1)))))
+       :else (recur (inc idx)))))
 
 ;; NOT thread-safe
 (defrecord Selection [^String alphabet ^Integer code-length ^booleans state]
@@ -29,29 +29,29 @@
   (next [this]
     (let [first-false-index (first-false-index-of state)
           last-false-index (last-false-index-of state)]
-      (if (and (= last-false-index (- (alength state) 1))
+      (if (and (= last-false-index (dec (alength state)))
                (= first-false-index code-length))
         ;; [true, true, ..., true, false, false, ..., false]
         ;; no more valid code
         nil
-        (let [selected-false-index (if (= last-false-index (- (alength state) 1))
+        (let [selected-false-index (if (= last-false-index (dec (alength state)))
                                      (loop [selected (last-false-index-of state last-false-index)
                                             prev last-false-index]
-                                       (if (>= selected (- prev 1))
+                                       (if (>= selected (dec prev))
                                          (recur (last-false-index-of state selected)
                                                 selected)
                                          selected))
                                      last-false-index)]
           (aset state selected-false-index true)
-          (aset state (+ selected-false-index 1) false)
-          (let [nfalse (loop [i (+ 1 selected-false-index)
+          (aset state (inc selected-false-index) false)
+          (let [nfalse (loop [i (inc selected-false-index)
                               c 0]
                          (cond
                            (= i (alength state)) c
-                           (= false (aget state i)) (recur (+ i 1) (+ c 1))
-                           :else (recur (+ i 1) c)))]
+                           (= false (aget state i)) (recur (inc i) (inc c))
+                           :else (recur (inc i) c)))]
             (java.util.Arrays/fill state
-                                   (+ selected-false-index 1)
+                                   (inc selected-false-index)
                                    (+ selected-false-index nfalse 1)
                                    false)
             (java.util.Arrays/fill state
@@ -63,9 +63,9 @@
 (defrecord SelectionAllButOne [^String alphabet ^Integer index]
   solutions/Solution
   (current [this] (str (.substring alphabet 0 index)
-                       (.substring alphabet (+ index 1) (.length alphabet))))
+                       (.substring alphabet (inc index) (.length alphabet))))
   (next [this]
-    (let [new-index (+ index 1)]
+    (let [new-index (inc index)]
       (if (= new-index (.length alphabet))
         nil
         (SelectionAllButOne. alphabet new-index)))))
@@ -74,7 +74,7 @@
   solutions/Solution
   (current [this] (.charAt alphabet index))
   (next [this]
-    (let [new-index (+ index 1)]
+    (let [new-index (inc index)]
       (if (= new-index (.length alphabet))
         nil
         (SelectionOne. alphabet new-index)))))
@@ -90,7 +90,7 @@
       (<= length 0) (throw (IllegalArgumentException. (str "length must be greater than zero. Got " length ".")))
       (> length alphabet-length) (throw (IllegalArgumentException. (str "length must be less than or equal to length of alphabet. Got " length ".")))
       (= length alphabet-length) (SelectionAll. alphabet)
-      (= length (- alphabet-length 1)) (SelectionAllButOne. alphabet 0)
+      (= length (dec alphabet-length)) (SelectionAllButOne. alphabet 0)
       (= length 1) (SelectionOne. alphabet 0)
       :else (let [array (boolean-array alphabet-length)]
               (java.util.Arrays/fill array 0 (- alphabet-length length) false)
