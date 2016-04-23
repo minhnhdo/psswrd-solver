3e7eb4f83e539d33bb53c6d5eb2110cb552d1e03
Do Nhat Minh <mrordinaire@gmail.com>
2016-04-23 16:39:06 +0800

    updated selenium dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-webdriver "0.7.2"]
-                 [org.seleniumhq.selenium/selenium-server "2.49.0"]]
+                 [org.seleniumhq.selenium/selenium-java "2.52.0"]
+                 [org.seleniumhq.selenium/selenium-htmlunit-driver "2.52.0"]]
   :main ^:skip-aot psswrd-solver.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
